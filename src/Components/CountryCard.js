81026779e5034558dbb820daf021458d5fa71e23
Alkaison <505ganeshmourya@gmail.com>
2023-09-17 21:46:05 +0530

    Fix: added link component to the cards

--- src/Components/CountryCard.js
+++ src/Components/CountryCard.js
@@ -1,19 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CountryCard(props) {
   return (
-    <div className="country-card">
-      <div className="ctd-img">
-        <img src={props.flag} alt={`${props.name} Flag`} />
-      </div>
+    <Link to={`/${props.name}`}>
+      <div className="country-card">
+        <div className="ctd-img">
+          <img src={props.flag} alt={`${props.name} Flag`} />
+        </div>
 
-      <div className="ctd-info">
-        <p className="ctd-title">{props.name}</p>
-        <p className="ctd-desc"><strong>Population: </strong>{props.population}</p>
-        <p className="ctd-desc"><strong>Region: </strong>{props.region}</p>
-        <p className="ctd-desc"><strong>Capital: </strong>{props.capital}</p>
+        <div className="ctd-info">
+          <p className="ctd-title">{props.name}</p>
+          <p className="ctd-desc">
+            <strong>Population: </strong>
+            {props.population}
+          </p>
+          <p className="ctd-desc">
+            <strong>Region: </strong>
+            {props.region}
+          </p>
+          <p className="ctd-desc">
+            <strong>Capital: </strong>
+            {props.capital}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
