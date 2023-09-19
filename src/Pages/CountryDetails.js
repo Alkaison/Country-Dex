38f9c8404db4645a67e013b364b70fae213ed447
Alkaison <505ganeshmourya@gmail.com>
2023-09-20 00:13:09 +0530

    Feat: country details component fetcching from API

--- src/Pages/CountryDetails.js
+++ src/Pages/CountryDetails.js
@@ -1,13 +1,122 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { IoArrowBack } from "react-icons/io5";
+import { useParams, useNavigate } from "react-router-dom";
+import Spinner from "../Components/Spinner";
+import "../Styles/CountryDetails.css";
 
 function CountryDetails() {
+  // useNavigate to change route/page path
+  const navigate = useNavigate();
   const { country } = useParams();
+  const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchNativeNames = (name) => Object.values(name.nativeName).map(language => language.common);
+  const fetchCurrencies = (name) => Object.values(name).map(currencies => `${currencies.name} (${currencies.symbol})`);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    fetch(`https://restcountries.com/v3.1/name/${country}?fullText=true`)
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`HTTP error! Status: ${response.status}`);
+
+        return response.json();
+      })
+      .then((data) => {
+        if (isMounted) {
+          setApiData(data[0]);
+          // console.log(data[0]);
+          setLoading(false);
+        }
+      })
+      .catch((error) => {
+        if (isMounted) {
+          console.log(`Fetch API Error: ${error}`);
+          setLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [country]);
 
   return (
-    <div className="country-details">
-      <p>Country Details Page - {country}</p>
-    </div>
+    <section className="country-details-section">
+      <div className="back-btn-container">
+        <button
+          type="button"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <IoArrowBack /> Back
+        </button>
+      </div>
+
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="countryInfoSection">
+          <div className="countryFlag-container">
+            <img src={apiData.flags.png} alt={`${apiData.name.common} Flag`} />
+          </div>
+
+          <div className="countryStats-container">
+            <p className="country-title">{apiData.name.common}</p>
+
+            <div className="country-details">
+              <div>
+                <p>
+                  <strong>Native Names: </strong>
+                  {fetchNativeNames(apiData.name).map((names) => `${names}`).join(", ")}
+                </p>
+                <p>
+                  <strong>Population: </strong>
+                  {apiData.population.toLocaleString()}
+                </p>
+                <p>
+                  <strong>Region: </strong>
+                  {apiData.region}
+                </p>
+                <p>
+                  <strong>Sub Region: </strong>
+                  {apiData.subregion}
+                </p>
+                <p>
+                  <strong>Capital: </strong>
+                  {apiData.capital.map((record) => `${record}`).join(", ")}
+                </p>
+              </div>
+
+              <div>
+                <p>
+                  <strong>Top Level Domain: </strong>
+                  {apiData.tld.map((record) => `${record}`).join(", ")}
+                </p>
+                <p>
+                  <strong>Currencies: </strong>
+                  {fetchCurrencies(apiData.currencies).map((record) => `${record}`).join(", ")}
+                </p>
+                <p>
+                  <strong>Languages: </strong>
+                  {Object.values(apiData.languages).map((record) => `${record}`).join(", ")}
+                </p>
+              </div>
+            </div>
+
+            <div className="country-border">
+              <p>
+                {apiData.borders && <strong>Border Countries: </strong>}
+                {apiData.borders ? apiData.borders.map((record, index) => <span key={index} className="country-border-name">{record}</span>) : <span className="no-countries">No Borders for this country...!</span>}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
   );
 }
 
